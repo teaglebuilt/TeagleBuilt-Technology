ccd70ae74e851b5116d1fbdb51cbb74f1c8d9e47
Dillan Teagle <dteagle@cpicardgroup.com>
2020-01-11 14:07:17 -0600

    add dependencies for docker

--- src/components/darkmode.js
+++ src/components/darkmode.js
@@ -1,13 +1,14 @@
 import React from 'react';
-
 import Toggle from './toggle';
 import useDarkMode from 'use-dark-mode';
+import classes from '../styles/index.sass'
+
 
 const DarkModeToggle = () => {
   const darkMode = useDarkMode(false, document.body.getRootNode());
 
   return (
-    <div className="dark-mode-toggle">
+    <>
       <button type="button" onClick={darkMode.disable}>
         ☀
       </button>
@@ -15,7 +16,7 @@ const DarkModeToggle = () => {
       <button type="button" onClick={darkMode.enable}>
         ☾
       </button>
-    </div>
+    </>
   );
 };
 
