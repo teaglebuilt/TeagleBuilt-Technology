08962524865acf9c2e973dc7c67ef289805387ae
Dillan Teagle <dteagle@cpicardgroup.com>
2020-01-20 13:31:33 -0600

    dev sync

---
+++ actions/devto/sync.py
@@ -0,0 +1,33 @@
+import os
+import requests
+import json
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+class DEVGateway:
+    url = "https://dev.to/api"
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.headers = {"api-key": os.environ["DEV_API_KEY"]}
+
+    def get_articles(self):
+        """Get all the published articles."""
+        response = self.session.get(
+            f"{self.url}/articles/me", params={"per_page": 1000}
+        )
+        response.raise_for_status()
+        return response.json()
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(levelname)s: %(message)s", level=logging.INFO, stream=sys.stdout
+    )
+    dev_gateway = DEVGateway()
+    articles = dev_gateway.get_articles()
+    logger.info(articles)
