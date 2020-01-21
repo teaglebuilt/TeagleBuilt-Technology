f34ccfdb117c21a798fe26148d2fd2e607feb9a3
Dillan Teagle <dteagle@cpicardgroup.com>
2020-01-21 13:53:59 -0600

    remove err

--- src/components/darkmode.js
+++ src/components/darkmode.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Toggle from './toggle';
 import useDarkMode from 'use-dark-mode';
-import classes from '../styles/index.sass'
 
 
 const DarkModeToggle = () => {
-  const darkMode = useDarkMode(false, document.body.getRootNode());
+  const darkMode = useDarkMode(false);
 
   return (
     <>
