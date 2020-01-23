4648c868319aab6edd025e1f25d6728e86833c89
Dillan Teagle <dteagle@cpicardgroup.com>
2020-01-23 12:15:18 -0600

    :rocket: finally finished plugin

--- plugins/gatsby-source-github/gatsby-node.js
+++ plugins/gatsby-source-github/gatsby-node.js
@@ -7,7 +7,7 @@ const { createNodeFactory, generateNodeId } = createNodeHelpers({
 
 const RepoNode = createNodeFactory('RepoNode');
 
-exports.sourceNodes = async ({ actions }, configOptions ) => {
+exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }, configOptions ) => {
     const { createNode } = actions
     delete configOptions.plugins
     const apikey = configOptions.key
@@ -33,68 +33,18 @@ exports.sourceNodes = async ({ actions }, configOptions ) => {
                 }
             })
             const readmeHTML = await readme.json()
-            let gitRepo = {
+            let newNode = {
+                id: createNodeId(`repoNode-${index}`),
                 url: `https://github.com/${repo}`,
                 description: repoData.description,
                 languages: languageData,
-                readme: readmeHTML.content
+                readme: readmeHTML.content,
+                internal: {
+                    type: 'RepoNode',
+                    contentDigest: createContentDigest('repoNode')
+                }
             }
-            console.log(gitRepo)
-            const repoNode = await RepoNode(gitRepo)
-            await createNode(repoNode)
-            // fetchedRepos.push(gitRepo)
-            // const githubRepo = RepoNode(node)
-            // createNode(githubRepo)
+            createNode(newNode)
         })
     )
 };
-
-
-
-// exports.sourceNodes = async (
-//     { actions, createNodeId, createContentDigest }, configOptions
-// ) => {
-//     const { createNode } = actions
-//     const options = { ...configOptions }
-//     await options.repos.forEach((repo, index, createNodeId, createContentDigest) => {
-//         const data = githubData(repo, options, index, createNodeId, createContentDigest)
-//         console.log(data)
-//     })
-// }
-
-
-// const githubData = async (repo, options, createNodeId, createContentDigest, index) => {
-//     console.log(repo, options);
-//     const param = queryString.stringify(repo)
-//     const response = await fetch(`https://api.github.com/repos/${repo}`, {
-//         headers: {
-//             Authorization: `token ${options.key}`
-//         }
-//     })
-//     const result = await response.json()
-//     const languages = await fetch(`https://api.github.com/repos/${repo}/languages`, {
-//         headers: {
-//             Authorization: `token ${options.key}`
-//         }
-//     })
-//     const languageData = await languages.json()
-//     const readme = await fetch(`https://api.github.com/repos/${repo}/readme`, {
-//         headers: {
-//             Authorization: `token ${options.key}`
-//         }
-//     })
-//     const readmeHTML = await readme.json()
-//     const data = {
-//         url: `https://github.com/${repo}`,
-//         description: result.description,
-//         languages: languageData,
-//         readme: readmeHTML
-//     }
-//     const node = {
-//         id: createNodeId(`Github-${index}`),
-//         internal: {
-//             type: "GithubRepo",
-//             contentDigest: createContentDigest(data),
-//         }
-//     }
-// }
