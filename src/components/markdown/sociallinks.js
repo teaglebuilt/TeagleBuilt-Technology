5d60d7eb93174009ee81b3558c618c6baad647dd
Dillan Teagle <dteagle@cpicardgroup.com>
2020-01-26 09:36:54 -0600

    add sociallinks

--- src/components/markdown/sociallinks.js
+++ src/components/markdown/sociallinks.js
@@ -7,23 +7,27 @@ import {
   } from "react-share";
 import urljoin from "url-join";
 import config from "../../../config/site"
-import classes from '../../styles/sociallinks.sass'
+import classes from '../../styles/sociallinks.module.sass'
 
 
-  class SocialLinks extends React.Component {
+class SocialLinks extends React.Component {
 
     render() {
         const { title, path, description } = this.props;
         const url = urljoin(config.siteUrl, "/", path)
         return(
-            <div className={classes.social_links}>
-                <TwitterShareButton url={url} title={title}>
-                    <TwitterIcon square borderRadius={10} size={48} />
-                </TwitterShareButton>
-                <LinkedinShareButton url={url} title={title} description={description}>
-                    <LinkedinIcon square borderRadius={10} size={48} />
-                </LinkedinShareButton>
-            </div>
+            <div className={classes.share_container}>
+                <div className={classes.share_banner}>
+                    <div className={classes.line}></div>
+                    <TwitterShareButton url={url} title={title}>
+                        <TwitterIcon square borderRadius={10} size={48} />
+                    </TwitterShareButton>
+                    <LinkedinShareButton url={url} title={title} description={description}>
+                        <LinkedinIcon square borderRadius={10} size={48} />
+                    </LinkedinShareButton>
+                    <div className={classes.line}></div>
+                </div>
+        </div> 
         )
     }
   }
